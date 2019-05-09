565f97c72ceba79c8d8a87fe67dfeddb11ebadc6
--replace-all <se.jones4@gmail.com>
2019-05-09 14:44:44 -0600

    first commit

---
+++ eightball.rb
@@ -0,0 +1,20 @@
+# Basic Objectives:
+
+# User inputs question
+# Computer outputs random answer
+# User inputs "QUIT"
+# Computer outputs a goodbye message and exits
+# Ability to add more answers:
+# Via easter egg question ("add_answers")
+# Do not let them add the same answer if the eight ball already has that answer
+# Ability to reset answers back to the original bank (hint: think arr.clone)
+# Via easter egg question ("reset_answers")
+# Ability to have eight ball print all answers
+# Via easter egg question ("print_answers")
+ 
+
+# Bonus Objectives:
+
+# Create a menu to handle all the functionality.
+# Use classes to handle the Eight Ball Program and Answers.
+# Use the colorize gem (from lecture) to add some color to your application.
